110d2a043fac6bc5a22174ee9b1054a58ab889bf
sandra <sandra.luengo@evoluciona.com>
2020-01-17 13:13:42 +0100

    test level 1.4

--- src/collision.js
+++ src/collision.js
@@ -6,7 +6,7 @@ const drawCollision = (
   ctx.setLineDash([5, 15]);
   ctx.arc(collisionX, collisionY, collisionRadio, 0, Math.PI * 2);
   ctx.closePath();
-  ctx.strokeStyle = 'red';
+  ctx.strokeStyle = "red";
   ctx.stroke();
 };
 
@@ -28,27 +28,33 @@ const createCollisions = ({ collisionArray, width, height }) => {
 };
 
 const isCollisionLeft = ({ ballArray, collisionArray }) => {
-  return ballArray.some(ball => collision( ball, collisionArray, 0 )); 
+  return ballArray.some(ball => collision(ball, collisionArray, 0));
 };
 
 const isCollisionRight = ({ ballArray, collisionArray }) => {
-   return ballArray.some(ball => collision( ball, collisionArray, 1 ));
+  return ballArray.some(ball => collision(ball, collisionArray, 1));
 };
 
 const collision = (ball, collisionArray, position) => {
-  let ballTop =  ball.ballY  - ball.ballRadius;
-  let ballBottom =  ball.ballY  + ball.ballRadius;
-  let collisionTop = collisionArray[position].collisionY - collisionArray[position].collisionRadio;
-  let collisionBottom = collisionArray[position].collisionY + collisionArray[position].collisionRadio;
+  let ballTop = ball.ballY - ball.ballRadius;
+  let ballBottom = ball.ballY + ball.ballRadius;
+  let collisionTop =
+    collisionArray[position].collisionY -
+    collisionArray[position].collisionRadio;
+  let collisionBottom =
+    collisionArray[position].collisionY +
+    collisionArray[position].collisionRadio;
   let ballRight = ball.ballX - ball.ballRadius;
   let ballLeft = ball.ballX + ball.ballRadius;
-  let collisionRight = collisionArray[position].collisionX - collisionArray[position].collisionRadio;
-  let collisionLeft = collisionArray[position].collisionX + collisionArray[position].collisionRadio;
+  let collisionRight =
+    collisionArray[position].collisionX -
+    collisionArray[position].collisionRadio;
+  let collisionLeft =
+    collisionArray[position].collisionX +
+    collisionArray[position].collisionRadio;
   return (
-    ballTop > collisionTop &&
-    ballTop < collisionBottom &&
     ballBottom > collisionTop &&
-    ballBottom < collisionBottom &&
+    ballTop < collisionBottom &&
     ballRight > collisionRight &&
     ballRight < collisionLeft &&
     ballLeft > collisionRight &&
